05dd030890652ae5582b6aa4f13260d2e6c3e80b
Nep <spica@starry.blue>
2017-09-12 08:37:06 +0900

    Refresh: Status, Userの未知の属性を更新

--- src/main/kotlin/jp/nephy/penicillin/api/model/User.kt
+++ src/main/kotlin/jp/nephy/penicillin/api/model/User.kt
@@ -5,41 +5,42 @@ import com.google.gson.JsonElement
 import jp.nephy.penicillin.api.*
 
 open class User(val json: JsonElement) {
-    val businessProfileState by json.byString("business_profile_state")
-    val canMediaTag by json.byBool("can_media_tag")
+    val businessProfileState by json.byNullableString("business_profile_state")
+    val canMediaTag by json.byNullableBool("can_media_tag")
     val contributorsEnabled by json.byBool("contributors_enabled")
     val createdAt by json.byCreatedAt("created_at")
     val defaultProfile by json.byBool("default_profile")
     val defaultProfileImage by json.byBool("default_profile_image")
     val description by json.byNullableString
-    val entities by json.byUserEntity
-    val fastFollowersCount by json.byInt("fast_followers_count")
+    val entities by json.byNullableUserEntity
+    val fastFollowersCount by json.byNullableInt("fast_followers_count")
     val favouritesCount by json.byInt("favourites_count")
     val followRequestSent by json.byNullableBool("follow_request_sent")
-    val followedBy by json.byBool("followed_by")
+    val followedBy by json.byNullableBool("followed_by")
     val followersCount by json.byInt("followers_count")
     val following by json.byNullableBool
     val friendsCount by json.byInt("friends_count")
     val geoEnabled by json.byBool("geo_enabled")
-    val hasCustomTimelines by json.byBool("has_custom_timelines")
-    val hasExtendedProfile by json.byBool("has_extended_profile")
+    val hasCustomTimelines by json.byNullableBool("has_custom_timelines")
+    val hasExtendedProfile by json.byNullableBool("has_extended_profile")
     val id by json.byLong
     val idStr by json.byString("id_str")
-    val isTranslationEnabled by json.byBool("is_translation_enabled")
+    val isTranslationEnabled by json.byNullableBool("is_translation_enabled")
     val isTranslator by json.byBool("is_translator")
     val lang by json.byLanguage
     val listedCount by json.byInt("listed_count")
     val location by json.byNullableString
-    val mediaCount by json.byInt("media_count")
+    val mediaCount by json.byNullableInt("media_count")
     val name by json.byString
-    val normalFollowersCount by json.byInt("normal_followers_count")
+    val normalFollowersCount by json.byNullableInt("normal_followers_count")
     val notifications by json.byNullableBool
-    val pinnedTweetIds by json.byStatusIDArray("pinned_tweet_ids")
-    val pinnedTweetIdsStr by json.byStringArray("pinned_tweet_ids_str")
+    val pinnedTweetIds by json.byNullableStatusIDArray("pinned_tweet_ids")
+    val pinnedTweetIdsStr by json.byNullableStringArray("pinned_tweet_ids_str")
     val profileBackgroundColor by json.byString("profile_background_color")
     val profileBackgroundImageUrl by json.byNullableURL("profile_background_image_url")
     val profileBackgroundImageUrlHttps by json.byNullableURL("profile_background_image_url_https")
     val profileBackgroundTile by json.byBool("profile_background_tile")
+    val profileBannerUrl by json.byNullableURL("profile_banner_url")
     val profileImageUrl by json.byURL("profile_image_url")
     val profileImageUrlHttps by json.byURL("profile_image_url_https")
     val profileLinkColor by json.byString("profile_link_color")
