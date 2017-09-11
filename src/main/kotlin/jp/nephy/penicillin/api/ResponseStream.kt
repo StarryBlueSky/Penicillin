f057657c8bad7c9983c8289d3132de4d8fc5bf5e
SlashNephy <slash@nephy.jp>
2017-09-12 08:38:06 +0900

    Add: UserStream処理用にレスポンスクラスを追加

---
+++ src/main/kotlin/jp/nephy/penicillin/api/ResponseStream.kt
@@ -0,0 +1,14 @@
+package jp.nephy.penicillin.api
+
+import okhttp3.Request
+import okhttp3.Response
+
+data class ResponseStream(val request: Request, val response: Response) {
+    fun print() {
+        println(request.toString())
+        println(request.headers())
+        println()
+        println(response.toString())
+        println(response.headers())
+    }
+}
