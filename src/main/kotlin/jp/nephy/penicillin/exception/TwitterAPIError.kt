2b7c3a601226c14a14c7fd2f5527b0f8f25543fb
SlashNephy <slash@nephy.jp>
2017-09-12 19:51:55 +0900

    Add: exceptionパッケージを追加

--- src/main/kotlin/jp/nephy/penicillin/exception/TwitterAPIError.kt
+++ src/main/kotlin/jp/nephy/penicillin/exception/TwitterAPIError.kt
@@ -1,8 +1,8 @@
-package jp.nephy.penicillin.api
+package jp.nephy.penicillin.exception
 
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import jp.nephy.penicillin.api.model.ErrorResponse
+import jp.nephy.penicillin.model.ErrorResponse
 
 class TwitterAPIError(msg: String, val content: String): Exception() {
     override var message = "$msg\n`$content` returned."
