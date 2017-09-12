c30628e32a4296121970464c8805be44a31d6929
SlashNephy <slash@nephy.jp>
2017-09-13 04:18:59 +0900

    Add: SiteStream, PublicStreamに対応

--- src/main/kotlin/jp/nephy/penicillin/endpoint/Stream.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoint/Stream.kt
@@ -2,6 +2,7 @@ package jp.nephy.penicillin.endpoint
 
 import jp.nephy.penicillin.Client
 import jp.nephy.penicillin.annotation.GET
+import jp.nephy.penicillin.annotation.POST
 import jp.nephy.penicillin.parameters.UserStreamFilterLevel
 import jp.nephy.penicillin.parameters.UserStreamReplies
 import jp.nephy.penicillin.parameters.UserStreamWith
@@ -37,6 +38,52 @@ class Stream(private val client: Client) {
                 .param("include_followings_activity" to includeFollowingsActivity)
                 .params(*options)
                 .get()
+                .getResponseStream()
+    }
+
+    @GET
+    fun getSiteStream(delimited: String?=null, stallWarnings: Boolean?=null, with: UserStreamWith?=null, replies: UserStreamReplies?=null, follow: Array<Long>?=null, vararg options: Pair<String, String?>): ResponseStream {
+        return client.session.new()
+                .url("https://sitestream.twitter.com/1.1/site.json")
+                .param("delimited" to delimited)
+                .param("stall_warning" to stallWarnings)
+                .param("with" to when (with) {
+                    UserStreamWith.User -> "user"
+                    UserStreamWith.Followings -> "followings"
+                    else -> null
+                })
+                .param("replies" to when (replies) {
+                    UserStreamReplies.All -> "all"
+                    else -> null
+                })
+                .param("follow" to follow?.joinToString(","))
+                .params(*options)
+                .get()
+                .getResponseStream()
+    }
+
+    @GET
+    fun getSampleStream(delimited: String?=null, stallWarnings: Boolean?=null, vararg options: Pair<String, String?>): ResponseStream {
+        return client.session.new()
+                .url("https://stream.twitter.com/1.1/statuses/sample.json")
+                .param("delimited" to delimited)
+                .param("stall_warning" to stallWarnings)
+                .params(*options)
+                .get()
+                .getResponseStream()
+    }
+
+    @POST
+    fun getFilterStream(delimited: String?=null, stallWarnings: Boolean?=null, track: Array<String>?=null, follow: Array<Long>?=null, locations: Pair<Float, Float>?=null, vararg options: Pair<String, String?>): ResponseStream {
+        return client.session.new()
+                .url("https://stream.twitter.com/1.1/statuses/filter.json")
+                .dataAsForm("delimited" to delimited)
+                .dataAsForm("stall_warning" to stallWarnings)
+                .dataAsForm("track" to track?.joinToString(","))
+                .dataAsForm("follow" to follow?.joinToString(","))
+                .dataAsForm("locations" to locations?.toList()?.joinToString(","))
+                .params(*options)
+                .post()
                 .getResponseStream()
     }
 }
