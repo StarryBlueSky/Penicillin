51e13218a94249efa618ae2f7170b261a7cc4c10
Nep <spica@starry.blue>
2017-09-13 00:45:13 +0900

    Add: エンドポイントの引数のヒントを提供するparametersパッケージを追加

---
+++ src/main/kotlin/jp/nephy/penicillin/parameters/EmbedAlign.kt
@@ -0,0 +1,5 @@
+package jp.nephy.penicillin.parameters
+
+enum class EmbedAlign {
+    Left, Right, Center, None
+}
---
+++ src/main/kotlin/jp/nephy/penicillin/parameters/TrendExclude.kt
@@ -0,0 +1,5 @@
+package jp.nephy.penicillin.parameters
+
+enum class TrendExclude {
+    Hashtags, NONE
+}
