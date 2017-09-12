4ca7fb0730735b11dc1a21af234cff2ac55d1d7a
SlashNephy <slash@nephy.jp>
2017-09-13 03:44:50 +0900

    Add: 未知の定義ファイルを追加 (DM)

---
+++ src/main/kotlin/jp/nephy/penicillin/model/WelcomeMessage.kt
@@ -0,0 +1,5 @@
+package jp.nephy.penicillin.model
+
+import com.google.gson.JsonElement
+
+class WelcomeMessage(val json: JsonElement)
--- src/main/kotlin/jp/nephy/penicillin/result/DirectMessagesWelcomeMessagesShow.kt
+++
@@ -1,5 +0,0 @@
-package jp.nephy.penicillin.result
-
-import com.google.gson.JsonElement
-
-class DirectMessagesWelcomeMessagesShow(val json: JsonElement)
