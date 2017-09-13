354e44d36a55ee4524cb80c4eb5b3fb00d1cbd03
Nep <spica@starry.blue>
2017-09-13 09:51:51 +0900

    Add: ResponseStreamにListenerを受け付けるlisten()を実装

--- src/main/kotlin/jp/nephy/penicillin/response/ResponseStream.kt
+++ src/main/kotlin/jp/nephy/penicillin/response/ResponseStream.kt
@@ -1,10 +1,15 @@
 package jp.nephy.penicillin.response
 
 import jp.nephy.penicillin.Client
+import jp.nephy.penicillin.streaming.*
 import okhttp3.Request
 import okhttp3.Response
 
 data class ResponseStream(val client: Client, val request: Request, val response: Response) {
+    fun listen(listener: IUserStreamListener) = UserStreamReceiver(this, listener)
+    fun listen(listener: ISampleStreamListener) = SampleStreamReceiver(this, listener)
+    fun listen(listener: IFilterStreamListener) = FilterStreamReceiver(this, listener)
+
     fun print() {
         println(request.toString())
         println(request.headers())
---
+++ src/main/kotlin/jp/nephy/penicillin/streaming/FilterStreamReceiver.kt
@@ -0,0 +1,14 @@
+package jp.nephy.penicillin.streaming
+
+import com.google.gson.JsonObject
+import jp.nephy.penicillin.model.Delete
+import jp.nephy.penicillin.model.Status
+import jp.nephy.penicillin.response.ResponseStream
+
+class FilterStreamReceiver(response: ResponseStream, private val listener: IFilterStreamListener): AbsStreamingParser(response) {
+    override fun handle(json: JsonObject) = when {
+        json.has("text") -> listener.onStatus(Status(json))
+        json.has("delete") -> listener.onDelete(Delete(json))
+        else -> listener.onUnknownData(json.toString())
+    }
+}
