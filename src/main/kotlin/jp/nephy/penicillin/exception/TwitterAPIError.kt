88fbda5ed6f93901480ea9ac870127815f5d9348
SlashNephy <slash@nephy.jp>
2017-09-17 12:04:04 +0900

    Improve: Response内のエラーハンドリングを改善

--- src/main/kotlin/jp/nephy/penicillin/exception/TwitterAPIError.kt
+++ src/main/kotlin/jp/nephy/penicillin/exception/TwitterAPIError.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import com.google.gson.JsonObject
 import jp.nephy.penicillin.model.ErrorResponse
 
-class TwitterAPIError(msg: String, val content: String): Exception() {
-    override var message = "$msg\n`$content` returned."
-    var json: ErrorResponse = ErrorResponse(Gson().fromJson(content, JsonObject::class.java))
+class TwitterAPIError(msg: String, val content: String, val code: Int?=null, message: String?=null): Exception() {
+    override var message = message ?: "$msg\n`$content` returned."
 }
