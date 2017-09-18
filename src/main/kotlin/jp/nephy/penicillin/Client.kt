c357b32406221213f86ec813c667a18e8ac223b9
SlashNephy <slash@nephy.jp>
2017-09-19 02:14:13 +0900

    Improve: Session周りを扱いやすく改善

--- src/main/kotlin/jp/nephy/penicillin/Client.kt
+++ src/main/kotlin/jp/nephy/penicillin/Client.kt
@@ -1,41 +1,17 @@
 package jp.nephy.penicillin
 
-import jp.nephy.penicillin.credential.*
 import jp.nephy.penicillin.endpoint.*
 import jp.nephy.penicillin.endpoint.Collection
 import jp.nephy.penicillin.endpoint.List
 import jp.nephy.penicillin.misc.AuthorizationType
 
-class Client private constructor(ck: ConsumerKey?, cs: ConsumerSecret?, at: AccessToken?, ats: AccessTokenSecret?, token: BearerToken?) {
+class Client(val session: Session) {
     companion object {
-        fun authenticate(ck: ConsumerKey?=null, cs: ConsumerSecret?=null, at: AccessToken?=null, ats: AccessTokenSecret?=null, token: BearerToken?=null) = Client(ck, cs, at, ats, token)
-    }
-
-    val session = Session(this)
-    var authType: AuthorizationType? = null
-
-    init {
-        if (ck != null && cs != null && at != null && ats != null) {
-            session.authenticate(ck, cs, at, ats)
-            authType = AuthorizationType.OAuth1a
-        } else if (ck != null && cs != null) {
-            session.authenticate(ck, cs)
-            authType = AuthorizationType.OAuth1aRequestToken
-        }
-        if (token != null) {
-            session.authenticate(token)
-            if (authType == null) {
-                authType = AuthorizationType.OAuth2
-            }
-        }
-
-        if (authType == null) {
-            throw IllegalArgumentException("ck, cs, at, ats, token are all null.")
-        }
+        fun builder() = ClientBuilder()
     }
 
     fun authType(type: AuthorizationType) = this.apply {
-        authType = type
+        session.authType = type
     }
 
     val account = Account(this)
@@ -52,6 +28,7 @@ class Client private constructor(ck: ConsumerKey?, cs: ConsumerSecret?, at: Acce
     val help = Help(this)
     val list = List(this)
     val media = Media(this)
+    val misc = Misc(this)
     val mute = Mute(this)
     val oauth = OAuth(this)
     val oauth2 = OAuth2(this)
