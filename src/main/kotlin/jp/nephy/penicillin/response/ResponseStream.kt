2a75e7d7b5ec1d40da930bc18614cae5b8e95b52
SlashNephy <slash@nephy.jp>
2017-09-19 02:15:01 +0900

    Change: Response型がclientを受け取らなくなった

--- src/main/kotlin/jp/nephy/penicillin/response/ResponseStream.kt
+++ src/main/kotlin/jp/nephy/penicillin/response/ResponseStream.kt
@@ -5,7 +5,7 @@ import jp.nephy.penicillin.streaming.*
 import okhttp3.Request
 import okhttp3.Response
 
-data class ResponseStream(val client: Client, val request: Request, val response: Response) {
+data class ResponseStream(val request: Request, val response: Response) {
     fun listen(listener: IUserStreamListener) = UserStreamReceiver(this, listener)
     fun listen(listener: ISampleStreamListener) = SampleStreamReceiver(this, listener)
     fun listen(listener: IFilterStreamListener) = FilterStreamReceiver(this, listener)
--- src/main/kotlin/jp/nephy/penicillin/response/ResponseText.kt
+++ src/main/kotlin/jp/nephy/penicillin/response/ResponseText.kt
@@ -5,7 +5,7 @@ import jp.nephy.penicillin.misc.RateLimit
 import okhttp3.Request
 import okhttp3.Response
 
-data class ResponseText(val content: String, val client: Client, val request: Request, val response: Response, val rateLimit: RateLimit) {
+data class ResponseText(val content: String, val request: Request, val response: Response, val rateLimit: RateLimit) {
     fun print() {
         println(request.toString())
         println(request.headers())
