6fd881b1f82785d690647f4c90cfe9a05cca4020
Nep <spica@starry.blue>
2017-09-21 12:04:44 +0900

    Fix: Trend関連のデータクラスを修正

--- src/main/kotlin/jp/nephy/penicillin/model/TrendModule.kt
+++ src/main/kotlin/jp/nephy/penicillin/model/TrendModule.kt
@@ -1,16 +1,15 @@
 package jp.nephy.penicillin.model
 
-import com.github.salomonbrys.kotson.byInt
-import com.github.salomonbrys.kotson.byString
-import com.github.salomonbrys.kotson.get
+import com.github.salomonbrys.kotson.*
 import com.google.gson.JsonElement
-import jp.nephy.penicillin.converter.byList
+import jp.nephy.penicillin.converter.byModel
 
 class TrendModule(val json: JsonElement) {
-    val name by json["trend"].byString
-    val description by json["trend"].byString("meta_description")
-    val rank by json["trend"].byInt
-    val token by json["trend"].byString
-    val relatedQueries by json["trend"]["context"].byList<String>("related_queries")
-    val targetQuery by json["trend"]["target"].byString("query")
+    val name by json.byString
+    val description by json.byNullableString("meta_description")
+    val rank by json.byInt
+    val token by json.byString
+
+    val context by json.byModel<TrendContext?>()
+    val target by json.byModel<TrendTarget>()
 }
