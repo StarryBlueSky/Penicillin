ff9029ecc8c97448348fe3aa8e05ad1474540e2c
Nep <spica@starry.blue>
2017-11-06 23:38:31 +0900

    Improve: 抽象API例外クラスを改善

--- src/main/kotlin/jp/nephy/penicillin/exception/AbsTwitterErrorMessage.kt
+++ src/main/kotlin/jp/nephy/penicillin/exception/AbsTwitterErrorMessage.kt
@@ -1,5 +1,17 @@
 package jp.nephy.penicillin.exception
 
-abstract class AbsTwitterErrorMessage(val code: Int, val text: String, val description: String): Exception() {
-    override val message = "${if (text.endsWith(".")) {text} else {text + "."}} ${if (description.endsWith(".")) {description} else {description + "."}} Returned with error code $code."
+abstract class AbsTwitterErrorMessage(val code: Int, val text: String, val description: String?): Exception() {
+    override val message = StringBuilder().apply {
+        append(text)
+        if (! text.endsWith(".")) {
+            append(".")
+        }
+        if (description != null) {
+            append(description)
+            if (! description.endsWith(".")) {
+                append(".")
+            }
+        }
+        append(" Returned with error code $code.")
+    }.toString()
 }
