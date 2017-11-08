1556027a0ca7c991402baf17ea58e1f50b861808
Slash Nephy <slash@nephy.jp>
2017-11-08 18:06:51 +0900

    Delete: 不要なクラスを削除

--- src/main/kotlin/jp/nephy/penicillin/model/ErrorResponse.kt
+++
@@ -1,8 +0,0 @@
-package jp.nephy.penicillin.model
-
-import com.google.gson.JsonElement
-import jp.nephy.penicillin.converter.byList
-
-class ErrorResponse(val json: JsonElement) {
-    val errors by json.byList<Error>()
-}
