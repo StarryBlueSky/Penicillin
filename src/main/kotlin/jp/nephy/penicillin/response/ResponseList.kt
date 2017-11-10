f362025b977f8088111f61678c8682d77e51c5b4
Slash Nephy <slash@nephy.jp>
2017-11-11 02:23:39 +0900

    @Suppress を追加

--- src/main/kotlin/jp/nephy/penicillin/response/ResponseList.kt
+++ src/main/kotlin/jp/nephy/penicillin/response/ResponseList.kt
@@ -3,4 +3,5 @@ package jp.nephy.penicillin.response
 import okhttp3.Request
 import okhttp3.Response
 
+@Suppress("UNUSED")
 class ResponseList<T>(content: String, request: Request, response: Response) : AbsRESTResponseList<T>(content, request, response)
--- src/main/kotlin/jp/nephy/penicillin/response/ResponseObject.kt
+++ src/main/kotlin/jp/nephy/penicillin/response/ResponseObject.kt
@@ -3,4 +3,5 @@ package jp.nephy.penicillin.response
 import okhttp3.Request
 import okhttp3.Response
 
+@Suppress("UNUSED")
 class ResponseObject<out T>(val result: T, content: String, request: Request, response: Response): AbsRESTResponse(content, request, response)
