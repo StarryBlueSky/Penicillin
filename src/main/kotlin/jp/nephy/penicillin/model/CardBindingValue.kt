75315fc7907e049aca30f129b8bad0da872d4f39
Slash Nephy <slash@nephy.jp>
2017-12-05 21:37:02 +0900

    Apply: byLambdaを適用しパフォーマンスを向上

--- src/main/kotlin/jp/nephy/penicillin/model/CardBindingValue.kt
+++ src/main/kotlin/jp/nephy/penicillin/model/CardBindingValue.kt
@@ -2,16 +2,19 @@ package jp.nephy.penicillin.model
 
 import com.github.salomonbrys.kotson.*
 import com.google.gson.JsonElement
+import jp.nephy.penicillin.converter.byLambda
 
 @Suppress("UNUSED")
 class CardBindingValue(val json: JsonElement) {
-    val choices = (1..5).filter { json.obj.contains("choice${it}_label") }.map {
-        json["choice${it}_label"]["string_value"].asString to if (json.obj.contains("choice${it}_count")) {
-            json["choice${it}_count"]["string_value"].asString
-        } else {
-            "0"
-        }.toInt()
-    }.toMap()
+    val choices by json.byLambda {
+        (1..5).filter { json.obj.contains("choice${it}_label") }.map {
+            json["choice${it}_label"]["string_value"].asString to if (json.obj.contains("choice${it}_count")) {
+                json["choice${it}_count"]["string_value"].asString
+            } else {
+                "0"
+            }.toInt()
+        }.toMap()
+    }
 
     val isFinal by json["counts_are_final"].byBool("boolean_value")
     val endAt by json["end_datetime_utc"].byString("string_value")
--- src/main/kotlin/jp/nephy/penicillin/model/Coordinate.kt
+++ src/main/kotlin/jp/nephy/penicillin/model/Coordinate.kt
@@ -2,6 +2,7 @@ package jp.nephy.penicillin.model
 
 import com.github.salomonbrys.kotson.byString
 import com.google.gson.JsonElement
+import jp.nephy.penicillin.converter.byLambda
 import jp.nephy.penicillin.converter.byList
 
 @Suppress("UNUSED")
@@ -9,14 +10,18 @@ class Coordinate(val json: JsonElement) {
     val coordinates by json.byList<Float>()
     val type by json.byString
 
-    val longitude = if (coordinates.size == 2) {
-        coordinates[0]
-    } else {
-        null
+    val longitude by json.byLambda {
+        if (coordinates.size == 2) {
+            coordinates[0]
+        } else {
+            null
+        }
     }
-    val latitude = if (coordinates.size == 2) {
-        coordinates[1]
-    } else {
-        null
+    val latitude by json.byLambda {
+        if (coordinates.size == 2) {
+            coordinates[1]
+        } else {
+            null
+        }
     }
 }
