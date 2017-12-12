959ae060d9abc1f700816031b65830476cdff3f8
Slash Nephy <slash@nephy.jp>
2017-12-13 00:10:06 +0900

    Add: IListenerのデフォルト実装を追加

--- src/main/kotlin/jp/nephy/penicillin/streaming/IUserStreamListener.kt
+++ src/main/kotlin/jp/nephy/penicillin/streaming/IUserStreamListener.kt
@@ -7,40 +7,40 @@ interface IUserStreamListener: IListener<IUserStreamListener> {
     override fun getReceiver(response: ResponseStream<IUserStreamListener>) = UserStreamReceiver(response, this)
 
     /* Status */
-    fun onStatus(status: Status)
-    fun onDirectMessage(message: DirectMessage)
+    fun onStatus(status: Status) {}
+    fun onDirectMessage(message: DirectMessage) {}
 
     /* Status event */
-    fun onFavorite(event: StatusEvent)
-    fun onUnfavorite(event: StatusEvent)
-    fun onFavoritedRetweet(event: StatusEvent)
-    fun onRetweetedRetweet(event: StatusEvent)
-    fun onQuotedTweet(event: StatusEvent)
+    fun onFavorite(event: StatusEvent) {}
+    fun onUnfavorite(event: StatusEvent) {}
+    fun onFavoritedRetweet(event: StatusEvent) {}
+    fun onRetweetedRetweet(event: StatusEvent) {}
+    fun onQuotedTweet(event: StatusEvent) {}
 
     /* List event */
-    fun onListCreated(event: ListEvent)
-    fun onListDestroyed(event: ListEvent)
-    fun onListUpdated(event: ListEvent)
-    fun onListMemberAdded(event: ListEvent)
-    fun onListMemberRemoved(event: ListEvent)
-    fun onListUserSubscribed(event: ListEvent)
-    fun onListUserUnsubscribed(event: ListEvent)
+    fun onListCreated(event: ListEvent) {}
+    fun onListDestroyed(event: ListEvent) {}
+    fun onListUpdated(event: ListEvent) {}
+    fun onListMemberAdded(event: ListEvent) {}
+    fun onListMemberRemoved(event: ListEvent) {}
+    fun onListUserSubscribed(event: ListEvent) {}
+    fun onListUserUnsubscribed(event: ListEvent) {}
 
     /* User event */
-    fun onFollow(event: UserEvent)
-    fun onUnfollow(event: UserEvent)
-    fun onBlock(event: UserEvent)
-    fun onUnblock(event: UserEvent)
-    fun onMute(event: UserEvent)
-    fun onUnmute(event: UserEvent)
-    fun onUserUpdate(event: UserEvent)
+    fun onFollow(event: UserEvent) {}
+    fun onUnfollow(event: UserEvent) {}
+    fun onBlock(event: UserEvent) {}
+    fun onUnblock(event: UserEvent) {}
+    fun onMute(event: UserEvent) {}
+    fun onUnmute(event: UserEvent) {}
+    fun onUserUpdate(event: UserEvent) {}
 
     /* Misc */
-    fun onFriends(friends: Friends)
-    fun onDelete(delete: Delete)
-    fun onScrubGeo(scrubGeo: ScrubGeo)
-    fun onStatusWithheld(withheld: StatusWithheld)
-    fun onLimit(limit: Limit)
+    fun onFriends(friends: Friends) {}
+    fun onDelete(delete: Delete) {}
+    fun onScrubGeo(scrubGeo: ScrubGeo) {}
+    fun onStatusWithheld(withheld: StatusWithheld) {}
+    fun onLimit(limit: Limit) {}
 
-    fun onUnknownData(data: String)
+    fun onUnknownData(data: String) {}
 }
