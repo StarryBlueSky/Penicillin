f630f6425e03dc2593907593c7099bb07b596bcb
Nep <spica@starry.blue>
2018-06-03 21:10:45 +0900

    Delete: Supports of Twitter for Windows, Twitter for WP

--- src/main/kotlin/jp/nephy/penicillin/OfficialClient.kt
+++ src/main/kotlin/jp/nephy/penicillin/OfficialClient.kt
@@ -3,9 +3,7 @@ package jp.nephy.penicillin
 enum class OfficialClient(val consumerKey: String, val consumerSecret: String) {
     TwitterForiPhone("IQKbtAYlXLripLGPWd0HUA", "GgDYlkSvaPxGxC4X8liwpUoqKwwr3lCADbz8A7ADU"),
     TwitterForAndroid("3nVuSoBZnx6U4vzUxf5w", "Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys"),
-    TwitterForiPad("CjulERsDeqhhjSme66ECg", "IQWdVyqFxghAtURHGeGiWAsmCAGmdW3WmbEx6Hck"),
-    TwitterForWindows("TgHNMa7WZE7Cxi1JbkAMQ", "SHy9mBMBPNj3Y17et9BF4g5XeqS4y3vkeW24PttDcY"),
-    TwitterForWindowsPhone("yN3DUNVO0Me63IAQdhTfCA", "c768oTKdzAjIYCmpSNIdZbGaG0t6rOhSFQP0S5uC79g");
+    TwitterForiPad("CjulERsDeqhhjSme66ECg", "IQWdVyqFxghAtURHGeGiWAsmCAGmdW3WmbEx6Hck");
 
     val appName: String
         get() = name.replace("For", " for ")
