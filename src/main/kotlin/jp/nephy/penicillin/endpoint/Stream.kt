def8ae8273c28823c3669e0347bd3aa7c3045608
Slash Nephy <slash@nephy.jp>
2018-08-19 17:51:35 +0900

    Improve: Streaming API

--- src/main/kotlin/jp/nephy/penicillin/endpoint/Stream.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoint/Stream.kt
@@ -8,21 +8,22 @@ import jp.nephy.penicillin.request.streaming.FilterStreamListener
 import jp.nephy.penicillin.request.streaming.SampleStreamListener
 import jp.nephy.penicillin.request.streaming.UserStreamListener
 
-
 class Stream(override val client: PenicillinClient): Endpoint {
-    fun userStream(delimited: String? = null, stallWarnings: Boolean? = null, with: UserStreamWith? = null, replies: UserStreamReplies? = null, track: List<String>? = null, filterLevel: UserStreamFilterLevel? = null, language: String? = null, follow: List<Long>? = null, locations: Pair<Float, Float>? = null, count: Int? = null, includeFollowingsActivity: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.getStream<UserStreamListener>("https://userstream.twitter.com/1.1/user.json") {
+    @Deprecated("UserStream API retired on August 23th, 2018.", replaceWith = ReplaceWith("Account Activity API (AAA)"))
+    fun user(delimited: String? = null, stallWarnings: Boolean? = null, with: UserStreamWith? = null, replies: UserStreamReplies? = null, track: List<String>? = null, filterLevel: UserStreamFilterLevel? = null, language: String? = null, follow: List<Long>? = null, locations: Pair<Float, Float>? = null, count: Int? = null, includeFollowingsActivity: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.getStream<UserStreamListener>("https://userstream.twitter.com/1.1/user.json") {
         query("delimited" to delimited, "stall_warning" to stallWarnings, "with" to with?.value, "replies" to replies?.value, "track" to track?.joinToString(","), "filter_level" to filterLevel?.value, "language" to language, "follow" to follow?.joinToString(","), "locations" to locations?.toList()?.joinToString(","), "count" to count, "include_followings_activity" to includeFollowingsActivity, *options)
     }
 
-    fun siteStream(delimited: String? = null, stallWarnings: Boolean? = null, with: UserStreamWith? = null, replies: UserStreamReplies? = null, follow: List<Long>? = null, vararg options: Pair<String, Any?>) = client.session.getStream<UserStreamListener>("https://sitestream.twitter.com/1.1/site.json") {
+    @Deprecated("SiteStream API retired on August 23th, 2018.", replaceWith = ReplaceWith("Account Activity API (AAA)"))
+    fun site(delimited: String? = null, stallWarnings: Boolean? = null, with: UserStreamWith? = null, replies: UserStreamReplies? = null, follow: List<Long>? = null, vararg options: Pair<String, Any?>) = client.session.getStream<UserStreamListener>("https://sitestream.twitter.com/1.1/site.json") {
         query("delimited" to delimited, "stall_warning" to stallWarnings, "with" to with?.value, "replies" to replies?.value, "follow" to follow?.joinToString(","), *options)
     }
 
-    fun sampleStream(delimited: String? = null, stallWarnings: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.getStream<SampleStreamListener>("https://stream.twitter.com/1.1/statuses/sample.json") {
+    fun sample(delimited: String? = null, stallWarnings: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.getStream<SampleStreamListener>("https://stream.twitter.com/1.1/statuses/sample.json") {
         query("delimited" to delimited, "stall_warning" to stallWarnings, *options)
     }
 
-    fun filterStream(delimited: String? = null, stallWarnings: Boolean? = null, track: List<String>? = null, follow: List<Long>? = null, locations: Pair<Float, Float>? = null, vararg options: Pair<String, Any?>) = client.session.postStream<FilterStreamListener>("https://stream.twitter.com/1.1/statuses/filter.json") {
+    fun filter(delimited: String? = null, stallWarnings: Boolean? = null, track: List<String>? = null, follow: List<Long>? = null, locations: Pair<Float, Float>? = null, vararg options: Pair<String, Any?>) = client.session.postStream<FilterStreamListener>("https://stream.twitter.com/1.1/statuses/filter.json") {
         form("delimited" to delimited, "stall_warning" to stallWarnings, "track" to track?.joinToString(","), "follow" to follow?.joinToString(","), "locations" to locations?.toList()?.joinToString(","), *options)
     }
 }
--- src/main/kotlin/jp/nephy/penicillin/request/streaming/SampleStream.kt
+++ src/main/kotlin/jp/nephy/penicillin/request/streaming/SampleStream.kt
@@ -9,10 +9,13 @@ import jp.nephy.penicillin.request.StreamAction
 typealias SampleStreamListener = SampleStream.Listener
 
 class SampleStream(action: StreamAction<SampleStreamListener>, listener: SampleStreamListener): StreamHandler<SampleStreamListener>(action, listener) {
-    override fun handle(json: JsonObject) = when {
-        json.contains("text") -> listener.onStatus(Status(json))
-        json.contains("delete") -> listener.onDelete(StreamDelete(json))
-        else -> listener.onUnknownData(json)
+    override fun handle(json: JsonObject) {
+        executor.execute { listener.onAnyData(json) }
+        when {
+            json.contains("text") -> listener.onStatus(Status(json))
+            json.contains("delete") -> listener.onDelete(StreamDelete(json))
+            else -> listener.onUnknownData(json)
+        }
     }
 
     interface Listener: StreamListener {
