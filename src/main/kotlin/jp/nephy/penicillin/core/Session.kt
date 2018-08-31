573ba53e1eb9f1c3bf286dc5901994c9d2c405b1
Slash Nephy <slash@nephy.jp>
2018-08-31 14:05:56 +0900

    Add http method

--- src/main/kotlin/jp/nephy/penicillin/core/Session.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/Session.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.http.HttpMethod
 import io.ktor.http.URLProtocol
 import jp.nephy.penicillin.core.auth.Credentials
+import jp.nephy.penicillin.endpoints.EndpointHost
 import java.io.Closeable
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -11,7 +12,7 @@ import java.util.concurrent.TimeUnit
 class Session(val httpClient: HttpClient, val credentials: Credentials, val option: ClientOption): Closeable {
     val executor = Executors.newCachedThreadPool()!!
 
-    fun call(method: HttpMethod, path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
+    private fun call(method: HttpMethod, path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
         return PenicillinRequestBuilder(this, method, protocol, host, path).apply(builder).build()
     }
 
@@ -23,8 +24,24 @@ class Session(val httpClient: HttpClient, val credentials: Credentials, val opti
         return call(HttpMethod.Post, path, host, protocol, builder)
     }
 
+    fun put(path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
+        return call(HttpMethod.Put, path, host, protocol, builder)
+    }
+
+    fun patch(path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
+        return call(HttpMethod.Patch, path, host, protocol, builder)
+    }
+
     fun delete(path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
         return call(HttpMethod.Delete, path, host, protocol, builder)
+    }
+
+    fun head(path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
+        return call(HttpMethod.Head, path, host, protocol, builder)
+    }
+
+    fun options(path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}): PenicillinRequest {
+        return call(HttpMethod.Options, path, host, protocol, builder)
     }
 
     override fun close() {
