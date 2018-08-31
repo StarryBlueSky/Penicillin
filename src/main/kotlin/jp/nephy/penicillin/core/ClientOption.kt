a9ec2549f25a88cbba9ce03ea5c2602ce2fc9d6a
Nep <spica@starry.blue>
2018-08-31 14:01:05 +0900

    Move ClientOption

--- src/main/kotlin/jp/nephy/penicillin/core/ClientOption.kt
+++
@@ -1,6 +0,0 @@
-package jp.nephy.penicillin.core
-
-import jp.nephy.penicillin.core.emulation.EmulationMode
-import java.util.concurrent.TimeUnit
-
-data class ClientOption(val maxRetries: Int, val retryInterval: Long, val retryIntervalUnit: TimeUnit, val emulationMode: EmulationMode)
