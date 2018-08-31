8b1d49f38ebd1b90cddaf94911058259ed48cb33
Slash Nephy <slash@nephy.jp>
2018-08-31 22:28:44 +0900

    Add support for new DM API

--- src/main/kotlin/jp/nephy/penicillin/endpoints/DirectMessageEvent.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/DirectMessageEvent.kt
@@ -1,22 +1,39 @@
 package jp.nephy.penicillin.endpoints
 
+import jp.nephy.jsonkt.jsonObject
 import jp.nephy.penicillin.PenicillinClient
-import jp.nephy.penicillin.models.DirectMessageEvent
+import jp.nephy.penicillin.models.DirectMessageEventList
+import jp.nephy.penicillin.models.DirectMessageEventShow
 
 class DirectMessageEvent(override val client: PenicillinClient): Endpoint {
-    fun create(vararg options: Pair<String, String>) = client.session.post("/1.1/direct_messages/events/new.json") {
+    fun create(userId: String, text: String, vararg options: Pair<String, String>) = client.session.post("/1.1/direct_messages/events/new.json") {
         body {
             json {
+                add("event" to jsonObject(
+                        "type" to "message_create",
+                        "message_create" to jsonObject(
+                                "target" to jsonObject(
+                                        "recipient_id" to userId
+                                ),
+                                "message_data" to jsonObject(
+                                        "text" to text
+                                )
+                        )
+                ))
                 add(*options)
             }
         }
-    }.empty()
+    }.jsonObject<DirectMessageEventShow>()
 
-    fun show(id: Long, vararg options: Pair<String, Any?>) = client.session.get("/1.1/direct_messages/events/show.json") {
+    fun delete(id: String, vararg options: Pair<String, Any?>) = client.session.delete("/1.1/direct_messages/events/destroy.json") {
         parameter("id" to id, *options)
-    }.jsonObject<DirectMessageEvent>()
+    }.text()
 
-    fun list(count: Int? = null, vararg options: Pair<String, Any?>) = client.session.get("/1.1/direct_messages/events/list.json") {
-        parameter("count" to count, *options)
-    }.jsonArray<DirectMessageEvent>()
+    fun list(count: Int? = null, cursor: String? = null, vararg options: Pair<String, Any?>) = client.session.get("/1.1/direct_messages/events/list.json") {
+        parameter("count" to count, "cursor" to cursor, *options)
+    }.jsonObject<DirectMessageEventList>()
+
+    fun show(id: String, vararg options: Pair<String, Any?>) = client.session.get("/1.1/direct_messages/events/show.json") {
+        parameter("id" to id, *options)
+    }.jsonObject<DirectMessageEventShow>()
 }
--- src/main/kotlin/jp/nephy/penicillin/models/DirectMessageEvent.kt
+++ src/main/kotlin/jp/nephy/penicillin/models/DirectMessageEvent.kt
@@ -1,5 +1,62 @@
 package jp.nephy.penicillin.models
 
 import com.google.gson.JsonObject
+import jp.nephy.jsonkt.*
 
-data class DirectMessageEvent(override val json: JsonObject): PenicillinModel
+data class DirectMessageEventList(override val json: JsonObject): PenicillinModel {
+    val apps by json.byModel<App>()
+    val events by json.byModelList<Event>()
+    val nextCursor by json.byNullableString("next_cursor")
+
+    data class App(override val json: JsonObject): PenicillinModel {
+        val id by lazy { json.keys.first() }
+        val name by json[id].byString
+        val url by json[id].byString
+    }
+
+    data class Event(override val json: JsonObject): PenicillinModel {
+        val createdTimestamp by json.byString("created_timestamp")
+        val id by json.byString
+        val messageCreate by json.byModel<MessageCreate>(key = "message_create")
+        val type by json.byString
+
+        data class MessageCreate(override val json: JsonObject): PenicillinModel {
+            val messageData by json.byModel<MessageData>(key = "message_data")
+            val senderId by json.byString("sender_id")
+            val sourceAppId by json.byNullableString("source_app_id")
+            val target by json.byModel<Target>()
+
+            data class MessageData(override val json: JsonObject): PenicillinModel {
+                val entities by json.byModel<Entities>()
+                val text by json.byString
+
+                data class Entities(override val json: JsonObject): PenicillinModel {
+                    val hashtags by json.byModelList<Hashtag>()
+                    val symbols by json.byJsonArray
+                    val urls by json.byModelList<Url>()
+                    val userMentions by json.byJsonArray("user_mentions")
+
+                    data class Hashtag(override val json: JsonObject): PenicillinModel {
+                        val indices by json.byIntList
+                        val text by json.byString
+                    }
+
+                    data class Url(override val json: JsonObject): PenicillinModel {
+                        val displayUrl by json.byString("display_url")
+                        val expandedUrl by json.byString("expanded_url")
+                        val indices by json.byIntList
+                        val url by json.byString
+                    }
+                }
+            }
+
+            data class Target(override val json: JsonObject): PenicillinModel {
+                val recipientId by json.byString("recipient_id")
+            }
+        }
+    }
+}
+
+data class DirectMessageEventShow(override val json: JsonObject): PenicillinModel {
+    val event by json.byModel<DirectMessageEventList.Event>()
+}
