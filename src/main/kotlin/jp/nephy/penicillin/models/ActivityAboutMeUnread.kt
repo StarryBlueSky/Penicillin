237da3ecdf03b619090e7abf853f857ca203c2bc
Nep <spica@starry.blue>
2018-09-05 17:29:16 +0900

    Update /activity/about_me

--- src/main/kotlin/jp/nephy/penicillin/models/ActivityAboutMeUnread.kt
+++
@@ -1,9 +0,0 @@
-package jp.nephy.penicillin.models
-
-import com.google.gson.JsonObject
-import jp.nephy.jsonkt.byLong
-
-
-data class ActivityAboutMeUnread(override val json: JsonObject): PenicillinModel {
-    val cursor by json.byLong
-}
