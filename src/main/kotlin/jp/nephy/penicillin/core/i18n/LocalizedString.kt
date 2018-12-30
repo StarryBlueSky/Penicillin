9554e8e4e282b08265973f42bac13e7b4594074a
Nep <spica@starry.blue>
2018-12-30 10:34:19 +0900

    Add check if session is closed

--- src/main/kotlin/jp/nephy/penicillin/core/i18n/LocalizedString.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/i18n/LocalizedString.kt
@@ -4,40 +4,43 @@ import java.util.*
 
 enum class LocalizedString(private val ja: String, private val en: String) {
     CredentialsAreAllNull(
-        "ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret, BearerTokenのすべてがnullです。", "ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret and BearerToken are all null."
+        "ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret, BearerToken のすべてが null です。", "ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret and BearerToken are all null."
     ),
     PrivateEndpointRequiresOfficialClientEmulation(
-        "非公開APIへのアクセスには公式クライアントのエミュレーションが必要です。", "Access to private apis requires official client's emulation."
+        "非公開 API へのアクセスには公式クライアントのエミュレーションが必要です。", "Access to private apis requires official client's emulation."
     ),
     CursorIsZero(
-        "カーソル(cursor)値は0です。", "cursor value is 0."
+        "cursor は 0 です。", "cursor value is 0."
     ),
     ApiRequestFailedLog(
-        "APIのリクエストに失敗しました。 (%s, %d/%d)", "Failed to request API. (%s, %d/%d)"
+        "API のリクエストに失敗しました。 (%s, %d/%d)", "Failed to request API. (%s, %d/%d)"
     ),
     ApiRequestFailed(
-        "APIのリクエストに失敗しました: %s", "Failed to request API: %s"
+        "API のリクエストに失敗しました: %s", "Failed to request API: %s"
     ),
     ApiReturnedNon200StatusCode(
-        "APIが2xxではないステータスコードを返却しました: %s %s", "API returned non-200 status code: %s %s"
+        "API が 2xx ではないステータスコードを返却しました: %s %s", "API returned non-200 status code: %s %s"
     ),
     JsonParsingFailed(
-        "JSONをパースする際に例外が発生しました: %s", "Failed to parse JSON: %s"
+        "JSON をパースする際に例外が発生しました: %s", "Failed to parse JSON: %s"
     ),
     JsonModelCastFailed(
-        "JSONをモデルクラスにキャストする際に例外が発生しました: %s\n%s", "Failed to cast JSON to model class: %s\n%s"
+        "JSON をモデルクラスにキャストする際に例外が発生しました: %s\n%s", "Failed to cast JSON to model class: %s\n%s"
     ),
     InvalidJsonReturned(
-        "不正なJSONが返却されました。\n%s", "Invalid JSON returned.\n%s"
+        "不正な JSON が返却されました。\n%s", "Invalid JSON returned.\n%s"
     ),
     UnknownApiError(
-        "不明なAPIエラーが発生しました。 (%d: %s)\n%s", "Unknown API error occurred. (%d: %s)\n%s"
+        "不明な API エラーが発生しました。 (%d: %s)\n%s", "Unknown API error occurred. (%d: %s)\n%s"
     ),
     UnknownApiErrorWithStatusCode(
-        "不明なAPIエラーが発生しました。 (HTTP %d)\n%s", "Unknown API error occurred. (HTTP %d)\n%s"
+        "不明な API エラーが発生しました。 (HTTP %d)\n%s", "Unknown API error occurred. (HTTP %d)\n%s"
     ),
     ExceptionInAsyncBlock(
         "非同期実行中にエラーが発生しました。", "Exception in async block."
+    ),
+    SessionAlreadyClosed(
+        "Session は既に閉じられています。", "Session is already closed."
     );
 
     fun format(vararg args: Any?): String {
