f879d34bca9305f77e78c7dca6924477bd46df27
Slash Nephy <slash@nephy.jp>
2018-12-30 10:34:19 +0900

    Add check if session is closed

--- src/main/kotlin/jp/nephy/penicillin/core/Session.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/Session.kt
@@ -6,13 +6,22 @@ import io.ktor.client.HttpClient
 import io.ktor.http.HttpMethod
 import io.ktor.http.URLProtocol
 import jp.nephy.penicillin.core.auth.Credentials
+import jp.nephy.penicillin.core.i18n.LocalizedString
 import jp.nephy.penicillin.endpoints.EndpointHost
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.isActive
 import mu.KLogger
 import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
-data class Session(val httpClient: HttpClient, override val coroutineContext: CoroutineContext, val logger: KLogger, val credentials: Credentials, val option: ClientOption): Closeable, CoroutineScope {
+data class Session(private val underlyingHttpClient: HttpClient, override val coroutineContext: CoroutineContext, val logger: KLogger, val credentials: Credentials, val option: ClientOption): Closeable, CoroutineScope {
+    val httpClient: HttpClient
+        get() = if (underlyingHttpClient.coroutineContext.isActive) {
+            underlyingHttpClient
+        } else {
+            throw PenicillinLocalizedException(LocalizedString.SessionAlreadyClosed)
+        }
+
     fun call(
         method: HttpMethod, path: String, host: EndpointHost = EndpointHost.Default, protocol: URLProtocol = URLProtocol.HTTPS, builder: PenicillinRequestBuilder.() -> Unit = {}
     ): PenicillinRequest {
@@ -48,6 +57,6 @@ data class Session(val httpClient: HttpClient, override val coroutineContext: Co
     }
 
     override fun close() {
-        httpClient.close()
+        underlyingHttpClient.close()
     }
 }
