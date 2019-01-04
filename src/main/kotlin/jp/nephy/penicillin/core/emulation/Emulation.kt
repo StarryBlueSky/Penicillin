12e5ad6a59a12ccc66cc4fd5a4b9ca28669857bb
Slash Nephy <slash@nephy.jp>
2019-01-04 09:57:37 +0900

    Emulation to interface

--- src/main/kotlin/jp/nephy/penicillin/core/emulation/Emulation.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/emulation/Emulation.kt
@@ -3,7 +3,8 @@ package jp.nephy.penicillin.core.emulation
 import io.ktor.http.Headers
 import jp.nephy.penicillin.core.session.Session
 
-abstract class Emulation {
-    abstract val session: Session
-    open val headers: Headers = Headers.Empty
+interface Emulation {
+    val session: Session
+    val headers: Headers
+        get() = Headers.Empty
 }
