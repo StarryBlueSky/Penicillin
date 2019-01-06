272b2830bd040f588704d070cfb421207deaafe0
Slash Nephy <slash@nephy.jp>
2019-01-06 22:28:05 +0900

    Add default timeout setting in SessionBuilder
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/extensions/ApiAction.kt
+++ src/main/kotlin/jp/nephy/penicillin/extensions/ApiAction.kt
@@ -15,6 +15,11 @@ suspend fun <R> ApiAction<R>.awaitWithTimeout(timeout: Long, unit: TimeUnit): R?
     }
 }
 
+@Throws(PenicillinException::class, CancellationException::class)
+suspend fun <R> ApiAction<R>.awaitWithTimeout(): R? {
+    return awaitWithTimeout(session.option.defaultTimeoutInMillis, TimeUnit.MILLISECONDS)
+}
+
 @Throws(PenicillinException::class)
 fun <R> ApiAction<R>.complete(): R {
     return runBlocking(session.coroutineContext) {
@@ -27,6 +32,11 @@ fun <R> ApiAction<R>.completeWithTimeout(timeout: Long, unit: TimeUnit): R? {
     return runBlocking(session.coroutineContext) {
         awaitWithTimeout(timeout, unit)
     }
+}
+
+@Throws(PenicillinException::class)
+fun <R> ApiAction<R>.completeWithTimeout(): R? {
+    return completeWithTimeout(session.option.defaultTimeoutInMillis, TimeUnit.MILLISECONDS)
 }
 
 private typealias ApiCallback<R> = suspend (response: R) -> Unit
@@ -85,4 +95,16 @@ inline fun <R> ApiAction<R>.queueWithTimeout(timeout: Long, unit: TimeUnit, cros
 
 fun <R> ApiAction<R>.queueWithTimeout(timeout: Long, unit: TimeUnit): Job {
     return queueWithTimeout(timeout, unit, defaultApiFallback, {})
+}
+
+inline fun <R> ApiAction<R>.queueWithTimeout(crossinline onFailure: ApiFallback, crossinline onSuccess: ApiCallback<R>): Job {
+    return queueWithTimeout(session.option.defaultTimeoutInMillis, TimeUnit.MILLISECONDS, onFailure, onSuccess)
+}
+
+inline fun <R> ApiAction<R>.queueWithTimeout(crossinline onSuccess: ApiCallback<R>): Job {
+    return queueWithTimeout(defaultApiFallback, onSuccess)
+}
+
+fun <R> ApiAction<R>.queueWithTimeout(): Job {
+    return queueWithTimeout(defaultApiFallback, {})
 }
