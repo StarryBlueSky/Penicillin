d75fcc362e7f579ac8d4a5f5303fd84893d4bf12
Nep <spica@starry.blue>
2019-01-22 23:40:23 +0900

    Add EmptyApiAction
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/core/request/ApiRequest.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/request/ApiRequest.kt
@@ -28,17 +28,12 @@ import jp.nephy.penicillin.core.request.action.*
 import jp.nephy.penicillin.core.session.Session
 import jp.nephy.penicillin.core.streaming.handler.StreamHandler
 import jp.nephy.penicillin.core.streaming.listener.StreamListener
-import jp.nephy.penicillin.models.Empty
 import jp.nephy.penicillin.models.PenicillinCursorModel
 import jp.nephy.penicillin.models.PenicillinModel
 
 class ApiRequest(val session: Session, val builder: ApiRequestBuilder) {
     inline fun <reified M: PenicillinModel> jsonObject(): JsonObjectApiAction<M> {
         return JsonObjectApiAction(this, M::class)
-    }
-
-    fun empty(): JsonObjectApiAction<Empty> {
-        return JsonObjectApiAction(this, Empty::class)
     }
 
     inline fun <reified M: PenicillinModel> jsonArray(): JsonArrayApiAction<M> {
@@ -51,6 +46,10 @@ class ApiRequest(val session: Session, val builder: ApiRequestBuilder) {
 
     fun text(): TextApiAction {
         return TextApiAction(this)
+    }
+
+    fun empty(): EmptyApiAction {
+        return EmptyApiAction(this)
     }
 
     fun <L: StreamListener, H: StreamHandler<L>> stream(): StreamApiAction<L, H> {
