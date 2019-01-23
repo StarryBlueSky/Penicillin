e319f4c558df789efb46810cb550eeaab799ce9f
Slash Nephy <slash@nephy.jp>
2019-01-23 20:33:33 +0900

    Separate Blocks endpoints
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/endpoints/Blocks.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/Blocks.kt
@@ -27,57 +27,23 @@
 package jp.nephy.penicillin.endpoints
 
 import jp.nephy.penicillin.PenicillinClient
-import jp.nephy.penicillin.core.session.get
-import jp.nephy.penicillin.core.session.post
-import jp.nephy.penicillin.models.CursorIds
-import jp.nephy.penicillin.models.CursorUsers
-import jp.nephy.penicillin.models.User
 
+/**
+ * Returns [Blocks] endpoint instance.
+ * 
+ * [Twitter API reference](https://developer.twitter.com/en/docs/accounts-and-users/mute-block-report-users/overview)
+ *
+ * @return New [Blocks] endpoint instance.
+ * @receiver Current [PenicillinClient] instance.
+ */
 val PenicillinClient.blocks: Blocks
     get() = Blocks(this)
 
-class Blocks(override val client: PenicillinClient): Endpoint {
-    fun listIds(stringifyIds: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.get("/1.1/blocks/ids.json") {
-        parameter("stringify_ids" to stringifyIds, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    fun list(includeEntities: Boolean? = null, skipStatus: Boolean? = null, vararg options: Pair<String, Any?>) = client.session.get("/1.1/blocks/list.json") {
-        parameter("include_entities" to includeEntities, "skip_status" to skipStatus, *options)
-    }.cursorJsonObject<CursorUsers>()
-
-    fun create(screenName: String, includeEntities: Boolean? = null, skipStatus: Boolean? = null, vararg options: Pair<String, Any?>) =
-        client.session.post("/1.1/blocks/create.json") {
-            body {
-                form {
-                    add("screen_name" to screenName, "include_entities" to includeEntities, "skip_status" to skipStatus, *options)
-                }
-            }
-        }.jsonObject<User>()
-
-    fun create(userId: Long, includeEntities: Boolean? = null, skipStatus: Boolean? = null, vararg options: Pair<String, Any?>) =
-            client.session.post("/1.1/blocks/create.json") {
-                body {
-                    form {
-                        add("user_id" to userId, "include_entities" to includeEntities, "skip_status" to skipStatus, *options)
-                    }
-                }
-            }.jsonObject<User>()
-
-    fun destroy(screenName: String, includeEntities: Boolean? = null, skipStatus: Boolean? = null, vararg options: Pair<String, Any?>) =
-        client.session.post("/1.1/blocks/destroy.json") {
-            body {
-                form {
-                    add("screen_name" to screenName, "include_entities" to includeEntities, "skip_status" to skipStatus, *options)
-                }
-            }
-        }.jsonObject<User>()
-
-    fun destroy(userId: Long, includeEntities: Boolean? = null, skipStatus: Boolean? = null, vararg options: Pair<String, Any?>) =
-            client.session.post("/1.1/blocks/destroy.json") {
-                body {
-                    form {
-                        add("user_id" to userId, "include_entities" to includeEntities, "skip_status" to skipStatus, *options)
-                    }
-                }
-            }.jsonObject<User>()
-}
+/**
+ * Collection of api endpoints related to blocks.
+ *
+ * @constructor Creates new [Blocks] endpoint instance.
+ * @param client Current [PenicillinClient] instance.
+ * @see PenicillinClient.blocks
+ */
+class Blocks(override val client: PenicillinClient): Endpoint
---
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/blocks/Create.kt
@@ -0,0 +1,100 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+@file:Suppress("UNUSED", "PublicApiImplicitType")
+
+package jp.nephy.penicillin.endpoints.blocks
+
+import jp.nephy.penicillin.core.request.action.JsonObjectApiAction
+import jp.nephy.penicillin.core.session.post
+import jp.nephy.penicillin.endpoints.Blocks
+import jp.nephy.penicillin.endpoints.EndpointOverloads
+import jp.nephy.penicillin.endpoints.Option
+import jp.nephy.penicillin.models.User
+
+/**
+ * Blocks the specified user from following the authenticating user. In addition the blocked user will not show in the authenticating users mentions or timeline (unless retweeted by another user). If a follow or friend relationship exists it is destroyed.
+ * 
+ * The URL pattern /version/block/create/:screen_name_or_user_id.format is still accepted but not recommended. As a sequence of numbers is a valid screen name we recommend using the screen_name or user_id parameter instead.
+ * 
+ * [Twitter API reference](https://developer.twitter.com/en/docs/accounts-and-users/mute-block-report-users/api-reference/post-blocks-create)
+ *
+ * @param screenName Required. The screen name of the potentially blocked user. Helpful for disambiguating when a valid screen name is also a user ID.
+ * @param includeEntities The entities node will not be included when set to false.
+ * @param skipStatus When set to either true, t or 1 statuses will not be included in the returned user objects.
+ * @param options Optional. Custom parameters of this request.
+ * @receiver [Blocks] endpoint instance.
+ * @return [JsonObjectApiAction] for [User] model.
+ * @see Blocks.createByUserId
+ */
+@EndpointOverloads
+fun Blocks.createByScreenName(
+    screenName: String,
+    includeEntities: Boolean? = null,
+    skipStatus: Boolean? = null,
+    vararg options: Option
+) = create(screenName, null, includeEntities, skipStatus, *options)
+
+/**
+ * Blocks the specified user from following the authenticating user. In addition the blocked user will not show in the authenticating users mentions or timeline (unless retweeted by another user). If a follow or friend relationship exists it is destroyed.
+ *
+ * The URL pattern /version/block/create/:screen_name_or_user_id.format is still accepted but not recommended. As a sequence of numbers is a valid screen name we recommend using the screen_name or user_id parameter instead.
+ *
+ * [Twitter API reference](https://developer.twitter.com/en/docs/accounts-and-users/mute-block-report-users/api-reference/post-blocks-create)
+ *
+ * @param userId Required. The ID of the potentially blocked user. Helpful for disambiguating when a valid user ID is also a valid screen name.
+ * @param includeEntities The entities node will not be included when set to false.
+ * @param skipStatus When set to either true, t or 1 statuses will not be included in the returned user objects.
+ * @param options Optional. Custom parameters of this request.
+ * @receiver [Blocks] endpoint instance.
+ * @return [JsonObjectApiAction] for [User] model.
+ * @see Blocks.createByScreenName
+ */
+@EndpointOverloads
+fun Blocks.createByUserId(
+    userId: Long,
+    includeEntities: Boolean? = null,
+    skipStatus: Boolean? = null,
+    vararg options: Option
+) = create(null, userId, includeEntities, skipStatus, *options)
+
+private fun Blocks.create(
+    screenName: String? = null,
+    userId: Long? = null,
+    includeEntities: Boolean? = null,
+    skipStatus: Boolean? = null,
+    vararg options: Option
+) = client.session.post("/1.1/blocks/create.json") {
+    body {
+        form {
+            add(
+                "screen_name" to screenName,
+                "user_id" to userId,
+                "include_entities" to includeEntities,
+                "skip_status" to skipStatus,
+                *options
+            )
+        }
+    }
+}.jsonObject<User>()
