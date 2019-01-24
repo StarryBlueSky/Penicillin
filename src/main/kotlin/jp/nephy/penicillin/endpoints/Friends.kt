b622c740b7dbc01e08f4978c7968102ff230d505
Slash Nephy <slash@nephy.jp>
2019-01-24 21:00:06 +0900

    Separate Friends endpoints
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/endpoints/Friends.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/Friends.kt
@@ -27,43 +27,23 @@
 package jp.nephy.penicillin.endpoints
 
 import jp.nephy.penicillin.core.session.ApiClient
-import jp.nephy.penicillin.core.session.get
-import jp.nephy.penicillin.models.CursorIds
-import jp.nephy.penicillin.models.CursorUsers
 
+/**
+ * Returns [Friends] endpoint instance.
+
+ * [Twitter API reference](https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/overview)
+ *
+ * @return New [Friends] endpoint instance.
+ * @receiver Current [ApiClient] instance.
+ */
 val ApiClient.friends: Friends
     get() = Friends(this)
 
-class Friends(override val client: ApiClient): Endpoint {
-    fun listIds(stringifyIds: Boolean? = null, count: Int? = null, vararg options: Option) = client.session.get("/1.1/friends/ids.json") {
-        parameter("stringify_ids" to stringifyIds, "count" to count, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    fun listIds(userId: Long, stringifyIds: Boolean? = null, count: Int? = null, vararg options: Option) = client.session.get("/1.1/friends/ids.json") {
-        parameter("user_id" to userId, "stringify_ids" to stringifyIds, "count" to count, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    fun listIds(screenName: String, stringifyIds: Boolean? = null, count: Int? = null, vararg options: Option) = client.session.get("/1.1/friends/ids.json") {
-        parameter("screen_name" to screenName, "stringify_ids" to stringifyIds, "count" to count, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    fun list(count: Int? = null, skipStatus: Boolean? = null, includeUserEntities: Boolean? = null, vararg options: Option) =
-        client.session.get("/1.1/friends/list.json") {
-            parameter("count" to count, "skip_status" to skipStatus, "include_user_entities" to includeUserEntities, *options)
-        }.cursorJsonObject<CursorUsers>()
-
-    fun list(userId: Long, count: Int? = null, skipStatus: Boolean? = null, includeUserEntities: Boolean? = null, vararg options: Option) =
-        client.session.get("/1.1/friends/list.json") {
-            parameter("user_id" to userId, "count" to count, "skip_status" to skipStatus, "include_user_entities" to includeUserEntities, *options)
-        }.cursorJsonObject<CursorUsers>()
-
-    fun list(screenName: String, count: Int? = null, skipStatus: Boolean? = null, includeUserEntities: Boolean? = null, vararg options: Option) =
-            client.session.get("/1.1/friends/list.json") {
-                parameter("screen_name" to screenName, "count" to count, "skip_status" to skipStatus, "include_user_entities" to includeUserEntities, *options)
-            }.cursorJsonObject<CursorUsers>()
-
-    @PrivateEndpoint
-    fun followingList(userId: Long? = null, screenName: String? = null, count: Int? = null, vararg options: Option) = client.session.get("/1.1/friends/following/list.json") {
-        parameter("user_id" to userId, "screen_name" to screenName, "count" to count, *options)
-    }.cursorJsonObject<CursorUsers>()
-}
+/**
+ * Collection of api endpoints related to friends (followings).
+ *
+ * @constructor Creates new [Friends] endpoint instance.
+ * @param client Current [ApiClient] instance.
+ * @see ApiClient.friends
+ */
+class Friends(override val client: ApiClient): Endpoint
