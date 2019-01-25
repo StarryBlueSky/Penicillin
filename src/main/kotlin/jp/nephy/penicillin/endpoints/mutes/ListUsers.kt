1e621d2fdf687570af554cf0b5506d3eedd1f20c
Slash Nephy <slash@nephy.jp>
2019-01-25 16:14:19 +0900

    Separate Mutes endpoints
    Signed-off-by: Slash Nephy <slash@nephy.jp>

---
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/mutes/ListUsers.kt
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+@file:Suppress("UNUSED", "PublicApiImplicitType")
+
+package jp.nephy.penicillin.endpoints.mutes
+
+import jp.nephy.penicillin.core.request.action.CursorJsonObjectApiAction
+import jp.nephy.penicillin.core.session.get
+import jp.nephy.penicillin.endpoints.Mutes
+import jp.nephy.penicillin.endpoints.Option
+import jp.nephy.penicillin.models.CursorUsers
+
+/**
+ * Returns an array of [user objects](https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object) the authenticating user has muted.
+ *
+ * [Twitter API reference](https://developer.twitter.com/en/docs/accounts-and-users/mute-block-report-users/api-reference/get-mutes-users-ids)
+ *
+ * @param cursor Causes the list of IDs to be broken into pages of no more than 5000 IDs at a time. The number of IDs returned is not guaranteed to be 5000 as suspended users are filtered out. If no cursor is provided, a value of -1 will be assumed, which is the first "page." The response from the API will include a previous_cursor and next_cursor to allow paging back and forth. See [Using cursors to navigate collections](https://developer.twitter.com/en/docs/basics/cursoring) for more information.
+ * @param includeEntities The entities node will not be included when set to false.
+ * @param skipStatus When set to either true, t or 1 statuses will not be included in the returned user objects.
+ * @param options Optional. Custom parameters of this request.
+ * @receiver [Mutes] endpoint instance.
+ * @return [CursorJsonObjectApiAction] for [CursorUsers] model.
+ */
+fun Mutes.listUsers(
+    cursor: Long? = null,
+    includeEntities: Boolean? = null,
+    skipStatus: Boolean? = null,
+    vararg options: Option
+) = client.session.get("/1.1/mutes/users/list.json") {
+    parameter(
+        "cursor" to cursor,
+        "include_entities" to includeEntities,
+        "skip_status" to skipStatus,
+        *options
+    )
+}.cursorJsonObject<CursorUsers>()
+
+/**
+ * Shorthand property to [Mutes.listUsers].
+ * @see Mutes.listUsers
+ */
+val Mutes.listUsers
+    get() = listUsers()
