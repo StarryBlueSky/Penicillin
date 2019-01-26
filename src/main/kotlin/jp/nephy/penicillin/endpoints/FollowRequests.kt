77631d6d58f21e90d1c080277a789a53f9aeef1d
Slash Nephy <slash@nephy.jp>
2019-01-26 16:31:46 +0900

    Separate FollowRequests endpoints
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/endpoints/FollowRequests.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/FollowRequests.kt
@@ -27,41 +27,21 @@
 package jp.nephy.penicillin.endpoints
 
 import jp.nephy.penicillin.core.session.ApiClient
-import jp.nephy.penicillin.core.session.get
-import jp.nephy.penicillin.core.session.post
-import jp.nephy.penicillin.models.CursorIds
-import jp.nephy.penicillin.models.User
 
+/**
+ * Returns [FollowRequests] endpoint instance.
+ * 
+ * @return New [FollowRequests] endpoint instance.
+ * @receiver Current [ApiClient] instance.
+ */
 val ApiClient.followRequests: FollowRequests
     get() = FollowRequests(this)
 
-class FollowRequests(override val client: ApiClient): Endpoint {
-    fun received(stringifyIds: Boolean? = null, vararg options: Option) = client.session.get("/1.1/friendships/incoming.json") {
-        parameter("stringify_ids" to stringifyIds, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    fun sent(stringifyIds: Boolean? = null, vararg options: Option) = client.session.get("/1.1/friendships/outgoing.json") {
-        parameter("stringify_ids" to stringifyIds, *options)
-    }.cursorJsonObject<CursorIds>()
-
-    @PrivateEndpoint
-    fun accept(screenName: String? = null, userId: Long? = null, vararg options: Option) = client.session.post("/1.1/friendships/accept.json") {
-        body {
-            form {
-                add(
-                    "ext" to "mediaColor",
-                    "include_entities" to "1",
-                    "include_profile_interstitial_type" to "true",
-                    "include_profile_location" to "true",
-                    "include_user_entities" to "true",
-                    "include_user_hashtag_entities" to "true",
-                    "include_user_mention_entities" to "true",
-                    "include_user_symbol_entities" to "true",
-                    "screen_name" to screenName,
-                    "user_id" to userId,
-                    *options
-                )
-            }
-        }
-    }.jsonObject<User>()
-}
+/**
+ * Collection of api endpoints related to follow requests.
+ *
+ * @constructor Creates new [FollowRequests] endpoint instance.
+ * @param client Current [ApiClient] instance.
+ * @see ApiClient.followRequests
+ */
+class FollowRequests(override val client: ApiClient): Endpoint
