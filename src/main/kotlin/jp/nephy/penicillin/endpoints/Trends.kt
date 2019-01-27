835017f8d5ff577b5de11466d8393463c67c3e91
Nep <spica@starry.blue>
2019-01-27 15:11:51 +0900

    Separate Trends endpoints
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/endpoints/Trends.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/Trends.kt
@@ -27,50 +27,23 @@
 package jp.nephy.penicillin.endpoints
 
 import jp.nephy.penicillin.core.session.ApiClient
-import jp.nephy.penicillin.core.session.get
-import jp.nephy.penicillin.endpoints.parameters.TrendExclude
-import jp.nephy.penicillin.models.TrendArea
-import jp.nephy.penicillin.models.TrendPlace
-import jp.nephy.penicillin.models.TrendPlus
 
+/**
+ * Returns [Trends] endpoint instance.
+
+ * [Twitter API reference](https://developer.twitter.com/en/docs/trends/trends-for-location)
+ *
+ * @return New [Trends] endpoint instance.
+ * @receiver Current [ApiClient] instance.
+ */
 val ApiClient.trends: Trends
     get() = Trends(this)
 
-class Trends(override val client: ApiClient): Endpoint {
-    fun availableAreas(vararg options: Option) = client.session.get("/1.1/trends/available.json") {
-        parameter(*options)
-    }.jsonArray<TrendArea>()
-
-    fun closestAreas(lat: Float, long: Float, vararg options: Option) = client.session.get("/1.1/trends/closest.json") {
-        parameter("lat" to lat, "long" to long, *options)
-    }.jsonArray<TrendArea>()
-
-    fun listPlaceTrends(id: Long, exclude: TrendExclude? = null, vararg options: Option) = client.session.get("/1.1/trends/place.json") {
-        parameter("id" to id, "exclude" to exclude?.value, *options)
-    }.jsonArray<TrendPlace>()
-
-    @PrivateEndpoint
-    fun trendPlus(vararg options: Option) = client.session.get("/1.1/trends/plus.json") {
-        parameter(
-            "cards_platform" to "iPhone-13",
-            "contributor_details" to "1",
-            "ext" to "altText,info360,mediaColor,mediaRestrictions,mediaStats,stickerInfo",
-            "include_cards" to "1",
-            "include_carousels" to "1",
-            "include_entities" to "1",
-            "include_ext_media_color" to "true",
-            "include_media_features" to "true",
-            "include_my_retweet" to "1",
-            "include_profile_interstitial_type" to "true",
-            "include_profile_location" to "true",
-            "include_reply_count" to "1",
-            "include_user_entities" to "true",
-            "include_user_hashtag_entities" to "true",
-            "include_user_mention_entities" to "true",
-            "include_user_symbol_entities" to "true",
-            "pc" to "true",
-            "tweet_mode" to "extended",
-            *options
-        )
-    }.jsonObject<TrendPlus>()
-}
+/**
+ * Collection of api endpoints related to trends.
+ *
+ * @constructor Creates new [Trends] endpoint instance.
+ * @param client Current [ApiClient] instance.
+ * @see ApiClient.trends
+ */
+class Trends(override val client: ApiClient): Endpoint
---
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/trends/Closest.kt
@@ -0,0 +1,58 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+@file:Suppress("UNUSED", "PublicApiImplicitType")
+
+package jp.nephy.penicillin.endpoints.trends
+
+import jp.nephy.penicillin.core.request.action.JsonArrayApiAction
+import jp.nephy.penicillin.core.session.get
+import jp.nephy.penicillin.endpoints.Option
+import jp.nephy.penicillin.endpoints.Trends
+import jp.nephy.penicillin.models.TrendArea
+
+/**
+ * Returns the locations that Twitter has trending topic information for, closest to a specified location.
+ * The response is an array of "locations" that encode the location's WOEID and some other human-readable information such as a canonical name and country the location belongs in.
+ * A WOEID is a [Yahoo! Where On Earth ID](http://developer.yahoo.com/geo/geoplanet/).
+ * 
+ * [Twitter API reference](https://developer.twitter.com/en/docs/trends/locations-with-trending-topics/api-reference/get-trends-closest)
+ * 
+ * @param latitude If provided with a longitude parameter the available trend locations will be sorted by distance, nearest to furthest, to the co-ordinate pair. The valid ranges for longitude is -180.0 to +180.0 (West is negative, East is positive) inclusive.
+ * @param longitude If provided with a latitude parameter the available trend locations will be sorted by distance, nearest to furthest, to the co-ordinate pair. The valid ranges for longitude is -180.0 to +180.0 (West is negative, East is positive) inclusive.
+ * @param options Optional. Custom parameters of this request.
+ * @receiver [Trends] endpoint instance.
+ * @return [JsonArrayApiAction] for [TrendArea] model.
+ */
+fun Trends.closestAreas(
+    latitude: Float,
+    longitude: Float,
+    vararg options: Option
+) = client.session.get("/1.1/trends/closest.json") {
+    parameter(
+        "lat" to latitude,
+        "long" to longitude,
+        *options
+    )
+}.jsonArray<TrendArea>()
