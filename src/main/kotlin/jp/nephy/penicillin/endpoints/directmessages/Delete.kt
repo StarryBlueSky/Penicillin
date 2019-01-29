c31a839749dee257712181b0c955a35dd66a194e
Slash Nephy <slash@nephy.jp>
2019-01-29 13:50:22 +0900

    Remove deprecation flags from DirectMessages
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/endpoints/directmessages/Delete.kt
+++ src/main/kotlin/jp/nephy/penicillin/endpoints/directmessages/Delete.kt
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-@file:Suppress("UNUSED", "PublicApiImplicitType", "Deprecation")
+@file:Suppress("UNUSED", "PublicApiImplicitType")
 
 package jp.nephy.penicillin.endpoints.directmessages
 
