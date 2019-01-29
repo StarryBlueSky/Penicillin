d83a7eb986b9e9fb6095116c9d338a9dbae77ac4
Slash Nephy <slash@nephy.jp>
2019-01-29 21:57:56 +0900

    PenicillinLocalizedException has LocalizedString parameter
    Signed-off-by: Slash Nephy <slash@nephy.jp>

--- src/main/kotlin/jp/nephy/penicillin/core/exceptions/PenicillinLocalizedException.kt
+++ src/main/kotlin/jp/nephy/penicillin/core/exceptions/PenicillinLocalizedException.kt
@@ -28,5 +28,5 @@ import io.ktor.client.request.HttpRequest
 import io.ktor.client.response.HttpResponse
 import jp.nephy.penicillin.core.i18n.LocalizedString
 
-class PenicillinLocalizedException(localizedString: LocalizedString, request: HttpRequest? = null, response: HttpResponse? = null, cause: Throwable? = null, vararg args: Any?):
+class PenicillinLocalizedException(val localizedString: LocalizedString, request: HttpRequest? = null, response: HttpResponse? = null, cause: Throwable? = null, vararg args: Any?):
     PenicillinException(localizedString.format(*args), request = request, response = response, cause = cause)
