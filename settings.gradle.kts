b6f8ca26a2976a2fc24dc60af362b26bb29c1a5c
Nep <spica@starry.blue>
2020-12-08 01:30:54 +0900

    migrate to Kotlin 1.4 changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,31 +24,10 @@
 
 rootProject.name = "penicillin"
 
-enableFeaturePreview("GRADLE_METADATA")
-
 pluginManagement {
     repositories {
         mavenCentral()
         jcenter()
         gradlePluginPortal()
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "com.jfrog.bintray" -> {
-                    useModule("com.jfrog.bintray.gradle:gradle-bintray-plugin:${requested.version}")
-                }
-                "org.jetbrains.dokka" -> {
-                    useModule("org.jetbrains.dokka:dokka-gradle-plugin:${requested.version}")
-                }
-                "com.adarshr.test-logger" -> {
-                    useModule("com.adarshr:gradle-test-logger-plugin:${requested.version}")
-                }
-                "build-time-tracker" -> {
-                    useModule("net.rdrei.android.buildtimetracker:gradle-plugin:${requested.version}")
-                }
-            }
-        }
     }
 }
