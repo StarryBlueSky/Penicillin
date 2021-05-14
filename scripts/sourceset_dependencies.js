6778b8cb107959c60b6c892b93a7c12f00cb4bff
SlashNephy <SlashNephy@users.noreply.github.com>
2021-05-14 12:05:17 +0000

    deploy: 98bbb18041f1470d6197a5c23385cf5bcbfb2a07

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":dokkaHtml/commonMain": [], ":dokkaHtml/jsMain": [":dokkaHtml/commonMain"], ":dokkaHtml/jvmMain": [":dokkaHtml/commonMain"]}'
+sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"]}'
